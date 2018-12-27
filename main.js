b182cefb01b525a090f7c99c3fbc74509cfa6801
kabo <kabo2468y@gmail.com>
2018-12-28 02:18:00 +0900

    音量ボタンの追加。動画を追加。ツイートボタン追加。
    その他修正。

--- main.js
+++ main.js
@@ -3,134 +3,165 @@ var eventText = '年越し';
 var videoListJson;
 
 function zeroPadding(num, length) {
-    'use strict';
-    return (Array(length).join('0') + num).slice(-length);
+	'use strict';
+	return (Array(length).join('0') + num).slice(-length);
 }
 
-// #region clock
 var dateDiff, updateOffset, nowDate;
 function formatDate(date) {
-    'use strict';
-    const y = date.getFullYear();
-    const m = date.getMonth() + 1;
-    const d = date.getDate();
-    const h = date.getHours();
-    const min = date.getMinutes();
-    const sec = date.getSeconds();
-    return `${y}年${m}月${d}日\n${h}時${zeroPadding(min, 2)}分${zeroPadding(sec, 2)}秒`;
+	'use strict';
+	const y = date.getFullYear();
+	const m = date.getMonth() + 1;
+	const d = date.getDate();
+	const h = date.getHours();
+	const min = date.getMinutes();
+	const sec = date.getSeconds();
+	return `${y}年${m}月${d}日\n${h}時${zeroPadding(min, 2)}分${zeroPadding(sec,2)}秒`;
 }
 
 function getDateOffset() {
-    'use strict';
-    const localDate = Date.now();
-    $.ajax({
-        type: "GET",
-        url: `https://ntp-a1.nict.go.jp/cgi-bin/json?${localDate / 1000}`,
-        dataType: "json"
-    }).done(function(res){
-        dateDiff = res.st * 1000 + (localDate - res.it * 1000) / 2 - localDate;
-    }).fail(function(){
-        dateDiff = 0;
-    });
-    updateOffset = true;
+	'use strict';
+	const localDate = Date.now();
+	$.ajax({
+		type: 'GET',
+		url: `https://ntp-a1.nict.go.jp/cgi-bin/json?${localDate / 1000}`,
+		dataType: 'json'
+	})
+		.done(function(res) {
+			dateDiff = res.st * 1000 + (localDate - res.it * 1000) / 2 - localDate;
+		})
+		.fail(function() {
+			dateDiff = 0;
+		});
+	updateOffset = true;
 }
 
 function clock() {
-    'use strict';
-    nowDate = new Date(Date.now() + dateDiff);
+	'use strict';
+	nowDate = new Date(Date.now() + dateDiff);
 
-    if (dateDiff !== undefined) {
-        $("#now-clock-text").html(formatDate(nowDate).replace(/\n/g, '<br>'));
-    }
+	if (dateDiff !== undefined) {
+		$('#now-clock-text').html(formatDate(nowDate).replace(/\n/g, '<br>'));
+	}
 
-    if (nowDate.getSeconds() === 30) {
-        if (updateOffset === false) {
-            getDateOffset();
-        }
-    } else {
-        updateOffset = false;
-    }
+	if (nowDate.getSeconds() === 30) {
+		if (updateOffset === false) {
+			getDateOffset();
+		}
+	} else {
+		updateOffset = false;
+	}
 
-    if (alarmDate < nowDate) {
-        $('#now-clock-text').addClass('red-text');
-    }
+	if (alarmDate < nowDate) {
+		$('#now-clock-text').addClass('red-text');
+	}
 
-    setTimeout(clock, 200);
+	setTimeout(clock, 200);
 }
-// #endregion clock
 
 (() => {
-    getDateOffset();
+	getDateOffset();
 })();
 
 $(() => {
-    'use strict';
-    const ua = navigator.userAgent;
-    if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0) {
-        // スマホ以外
-        $('#popup-pc, #container').remove();
-        $('#popup-content').attr('id', 'popup-content-wide');
-    } else {
-        $('#popup-other').remove();
-    }
+	'use strict';
+	const ua = navigator.userAgent;
+	if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0) {
+		// スマホ以外
+		$('#popup-pc, #container').remove();
+		$('#popup-content').attr('id', 'popup-content-wide');
+	} else {
+		$('#popup-other').remove();
+	}
 
-    $('.event-name').text(eventText);
-    $('#alarm-clock-text').html(formatDate(alarmDate).replace(/\n/g, '<br>'));
-    clock();
+	$('.event-name').text(eventText);
+	$('#alarm-clock-text').html(formatDate(alarmDate).replace(/\n/g, '<br>'));
+	clock();
 
-    $.getJSON('video-list.json', data => {
-        videoListJson = data;
-        let list;
-        for (let i in data) {
-            list = document.createElement('option');
-            list.text = data[i].name;
-            $('.selectList').append(list);
-        }
-    });
+	$.getJSON('video-list.json', data => {
+		videoListJson = data;
+		let list;
+		for (let i in data) {
+			list = document.createElement('option');
+			list.text = data[i].name;
+			$('.selectList').append(list);
+		}
+	});
 
-    $('#popup-layer, #popup-content, #popup-content-wide').show();
+	$('#popup-layer, #popup-content, #popup-content-wide').show();
 
-    $('#popup-close, #popup-layer').click(() => { 
-        $('#popup-layer, #popup-content, #popup-content-wide').remove();
-    });
+	$('#popup-close, #popup-layer').click(() => {
+		$('#popup-layer, #popup-content, #popup-content-wide').remove();
+	});
 });
 
 var Player = new Array(7);
 function onPlayerReady(event) {
-    event.target.pauseVideo();
+	event.target.pauseVideo();
 }
 
 function PlayerStart(player) {
-    player.playVideo();
+	player.playVideo();
 }
 
 $('.selectBtn').on('click', function() {
-    const select = $(this).prev('select');
-    const div = $(this).closest('div');
-    const divId = div.attr('id');
-    const num = Number(divId.replace('video', ''));
-    const idx = select.prop('selectedIndex');
+	const div = $(this).closest('div');
+	const divId = div.attr('id');
+	const num = Number(divId.replace('video', ''));
 
-    const video = videoListJson[idx];
-    Player[num] = new YT.Player(divId, {
-        width: div.width(),
-        height: div.height(),
-        videoId: video.id,
-        playerVars: {
-            start: video.start,
-            end: video.end,
-            control: 0,
-            showinfo: 0,
-            rel: 0,
-            autoplay: 1,
-            disablekb: 0,
-            fs: 0
-        },
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-    setTimeout(function() {
-        PlayerStart(Player[num]);
-    }, alarmDate - video.fit * 1000 - nowDate - 500);
+	const video = videoListJson[$(this).prev('select').prop('selectedIndex')];
+	Player[num] = new YT.Player(divId, {
+		width: div.width(),
+		height: div.height(),
+		videoId: video.id,
+		playerVars: {
+			start: video.start,
+			end: video.end,
+			control: 0,
+			showinfo: 0,
+			rel: 0,
+			autoplay: 1,
+			disablekb: 0,
+			fs: 0
+		},
+		events: {
+			onReady: onPlayerReady
+		}
+	});
+	setTimeout(function() {
+		PlayerStart(Player[num]);
+  }, alarmDate - video.fit * 1000 - video.start * 1000 - nowDate - 500);
+});
+
+$('#vol-mute').on('click', function() {
+  for(const i of Player) {
+    if (typeof i === 'undefined') {
+      continue;
+    }
+    if (i.isMuted()) {
+      i.unMute();
+    } else {
+      i.mute();
+    }
+  }
+});
+
+$('#vol-50').on('click', function() {
+  for (const i of Player) {
+    if (typeof i === 'undefined') {
+      continue;
+    }
+    i.unMute();
+    i.setVolume(50);
+  }
+});
+
+$('#vol-max').on('click', function() {
+  for (const i of Player) {
+    if (typeof i === 'undefined') {
+      continue;
+    }
+    i.unMute();
+    i.setVolume(100);
+  }
 });
--- single/single.js
+++ single/single.js
@@ -4,118 +4,120 @@ alarmDate.setHours(alarmDate.getHours() + 1);
 var videoListJson;
 
 function zeroPadding(num, length) {
-    'use strict';
-	return (Array(length).join('0') + num).slice(-length);
+  'use strict';
+  return (Array(length).join('0') + num).slice(-length);
 }
 
 var dateDiff, updateOffset, nowDate;
 function formatDate(date) {
-    'use strict';
-    const y = date.getFullYear();
-    const m = date.getMonth() + 1;
-    const d = date.getDate();
-    const h = date.getHours();
-    const min = date.getMinutes();
-    const sec = date.getSeconds();
-    return `${y}年${m}月${d}日\n${h}時${zeroPadding(min, 2)}分${zeroPadding(sec, 2)}秒`;
+  'use strict';
+  const y = date.getFullYear();
+  const m = date.getMonth() + 1;
+  const d = date.getDate();
+  const h = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  return `${y}年${m}月${d}日\n${h}時${zeroPadding(min, 2)}分${zeroPadding(sec,2)}秒`;
 }
 
 function getDateOffset() {
-    'use strict';
-    const localDate = Date.now();
-    $.ajax({
-        type: "GET",
-        url: `https://ntp-a1.nict.go.jp/cgi-bin/json?${localDate / 1000}`,
-        dataType: "json"
-    }).done(function(res){
-        dateDiff = res.st * 1000 + (localDate - res.it * 1000) / 2 - localDate;
-    }).fail(function(){
-        dateDiff = 0;
-    });
-    updateOffset = true;
+  'use strict';
+  const localDate = Date.now();
+  $.ajax({
+  type: 'GET',
+  url: `https://ntp-a1.nict.go.jp/cgi-bin/json?${localDate / 1000}`,
+  dataType: 'json'
+  })
+  .done(function(res) {
+    dateDiff = res.st * 1000 + (localDate - res.it * 1000) / 2 - localDate;
+  })
+  .fail(function() {
+    dateDiff = 0;
+  });
+  updateOffset = true;
 }
 
 function clock() {
-    'use strict';
-    nowDate = new Date(Date.now() + dateDiff);
+  'use strict';
+  nowDate = new Date(Date.now() + dateDiff);
 
-    if (dateDiff !== undefined) {
-        $("#nowDateText").html(formatDate(nowDate).replace(/\n/g, '<br>'));
+  if (dateDiff !== undefined) {
+    $('#nowDateText').html(formatDate(nowDate).replace(/\n/g, '<br>'));
+  }
+
+  if (nowDate.getSeconds() === 30) {
+    if (updateOffset === false) {
+      getDateOffset();
     }
+  } else {
+    updateOffset = false;
+  }
 
-    if (nowDate.getSeconds() === 30) {
-        if (updateOffset === false) {
-            getDateOffset();
-        }
-    } else {
-        updateOffset = false;
-    }
+  if (alarmDate < nowDate) {
+    $('#nowDateText').addClass('red-text');
+  } else {
+    $('#nowDateText').removeClass('red-text');
+  }
 
-    if (alarmDate < nowDate) {
-        $('#nowDateText').addClass('red-text');
-    } else {
-        $('#nowDateText').removeClass('red-text');
-    }
-
-    setTimeout(clock, 250);
+  setTimeout(clock, 250);
 }
 
 (() => {
-    getDateOffset();
+  getDateOffset();
 })();
 
 $(() => {
-    'use strict';
-    $('#setDateText').html(formatDate(alarmDate).replace(/\n/g, '<br>'));
+  'use strict';
+  $('#setDateText').html(formatDate(alarmDate).replace(/\n/g, '<br>'));
 
-    clock();
+  clock();
 
-    $.getJSON('../video-list.json', data => {
-        videoListJson = data;
-        let list;
-        for (let i in data) {
-            list = document.createElement('option');
-            list.text = data[i].name;
-            $('#selectList').append(list);
-        }
-    });
+  $.getJSON('../video-list.json', data => {
+    videoListJson = data;
+    let list;
+    for (let i in data) {
+      list = document.createElement('option');
+      list.text = data[i].name;
+      $('#selectList').append(list);
+    }
+  });
 });
 
-$('#applyBtn').on('click', function () {
-    alarmDate = new Date($('#setDateInput').val());
-    $('#setDateText').html(formatDate(alarmDate).replace(/\n/g, '<br>'));
+$('#applyBtn').on('click', function() {
+  alarmDate = new Date($('#setDateInput').val());
+  $('#setDateText').html(formatDate(alarmDate).replace(/\n/g, '<br>'));
 });
 
 var player;
 function onPlayerReady(event) {
-    event.target.pauseVideo();
+  event.target.pauseVideo();
 }
 
 function PlayerStart(player) {
-    player.playVideo();
+  player.playVideo();
 }
 
-$('#selectBtn').on('click', function () {
-    const idx = $('#selectList').prop('selectedIndex');
-    player = new YT.Player('youtube', {
-        width: 640,
-        height: 360,
-        videoId: videoListJson[idx].id,
-        playerVars: {
-            start: videoListJson[idx].start,
-            end: videoListJson[idx].end,
-            control: 0,
-            showinfo: 0,
-            rel: 0,
-            autoplay: 1,
-            disablekb: 0,
-            fs: 0
-        },
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-    setTimeout(function() {
-        PlayerStart(player);
-    }, alarmDate - videoListJson[idx].fit * 1000 - nowDate - 500);
+$('#selectBtn').on('click', function() {
+  const video = videoListJson[$('#selectList').prop('selectedIndex')];
+  player = new YT.Player('youtube', {
+    width: 640,
+    height: 360,
+    videoId: video.id,
+    playerVars: {
+      start: video.start,
+      end: video.end,
+      control: 0,
+      showinfo: 0,
+      rel: 0,
+      autoplay: 1,
+      disablekb: 0,
+      fs: 0
+    },
+    events: {
+      onReady: onPlayerReady
+    }
+  });
+  setTimeout(function() {
+    PlayerStart(player);
+  }, alarmDate - video.fit * 1000 - video.start * 1000 - nowDate - 500);
 });
