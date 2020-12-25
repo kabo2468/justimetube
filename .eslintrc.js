420a1f7c0f381998cb2b3dec55b88ba3eb4fe7c4
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2020-12-26 03:17:52 +0900

    Init TS

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "semi": "off",
+        "@typescript-eslint/semi": [
+            "error"
+        ],
+        "prefer-exponentiation-operator": "error",
+        "camelcase": "error",
+        "comma-spacing": "error",
+        "space-infix-ops": "error"
+    }
+};
