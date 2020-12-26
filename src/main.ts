122343bff695708482ee45e5cd602fc4b59e806b
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2020-12-27 03:32:12 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
-let videoListJson, alarmDate, eventText;
+import $ from 'jquery';
+import YTPlayer from 'youtube-player';
+import {YouTubePlayer} from "youtube-player/dist/types";
 
-const zeroPadding = (num, length) => {
-    'use strict';
+interface Video {
+    name: string;
+    id: string;
+    fit: number;
+    start: number;
+    end: number;
+}
+
+let videoListJson: Video[], alarmDate: Date, eventText: string;
+
+const zeroPadding = (num: number, length: number) => {
     return (Array(length).join('0') + num).slice(-length);
 };
 
-let dateDiff, nowDate;
-const formatDate = date => {
-    'use strict';
+let dateDiff = 0,
+    nowDate: Date;
+const formatDate = (date: Date) => {
     const y = date.getFullYear();
     const m = date.getMonth() + 1;
     const d = date.getDate();
@@ -18,23 +29,18 @@ const formatDate = date => {
 };
 
 const getDateOffset = () => {
-    'use strict';
     const localDate = Date.now();
-    $.ajax({
-        type: 'GET',
-        url: `https://ntp-a1.nict.go.jp/cgi-bin/json?${localDate / 1000}`,
-        dataType: 'json'
-    })
-        .done(res => {
-            dateDiff = res.st * 1000 + (localDate - res.it * 1000) / 2 - localDate;
+    fetch(`https://ntp-a1.nict.go.jp/cgi-bin/json?${localDate / 1000}`)
+        .then((res) => res.json())
+        .then((json) => {
+            dateDiff = json.st * 1000 + (localDate - json.it * 1000) / 2 - localDate;
         })
-        .fail(() => {
+        .catch(() => {
             dateDiff = 0;
         });
 };
 
 const clock = () => {
-    'use strict';
     nowDate = new Date(Date.now() + dateDiff);
 
     if (dateDiff !== undefined) {
@@ -55,7 +61,6 @@ const clock = () => {
 })();
 
 $(() => {
-    'use strict';
     $('#popup-layer, #popup-content, #popup-content-wide').show();
     const ua = navigator.userAgent;
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0) {
@@ -67,91 +72,75 @@ $(() => {
         $('#popup-other').remove();
     }
 
-    $.ajax({
-        type: 'GET',
-        cache: false,
-        url: 'event.json',
-        dataType: 'json'
+    fetch('event.json').then(res => res.json()).then(json => {
+        eventText = json.name;
+        alarmDate = new Date(json.date.year, json.date.month - 1, json.date.day, json.date.hour, json.date.minute, json.date.second);
+        if (Date.now() > alarmDate.getTime()) {
+            alarmDate = new Date(json.next.date.year, json.next.date.month - 1, json.next.date.day, json.next.date.hour, json.next.date.minute, json.next.date.second);
+            eventText = json.next.name;
+        }
+        $('.event-name').text(eventText);
+        $('#alarm-clock-text').html(formatDate(alarmDate).replace(/\n/g, ' '));
     })
-        .done(res => {
-            eventText = res.name;
-            alarmDate = new Date(res.date.year, res.date.month - 1, res.date.day, res.date.hour, res.date.minute, res.date.second);
-            if (Date.now() > alarmDate) {
-                alarmDate = new Date(res.next.date.year, res.next.date.month - 1, res.next.date.day, res.next.date.hour, res.next.date.minute, res.next.date.second);
-                eventText = res.next.name;
-            }
-            $('.event-name').text(eventText);
-            $('#alarm-clock-text').html(formatDate(alarmDate).replace(/\n/g, ' '));
-        });
     clock();
 
-    $.ajax({
-        type: 'GET',
-        cache: false,
-        url: 'video-list.json',
-        dataType: 'json'
+    fetch('video-list.json').then(res => res.json()).then(json => {
+        videoListJson = json;
+        updateOption(json);
     })
-        .done(res => {
-            videoListJson = res;
-            updateOption(res);
-        });
 });
 
-const updateOption = videoList => {
-    'use strict';
+const updateOption = (videoList: Video[]) => {
     const selectList = $('.selectList');
     selectList.empty();
-    videoList.forEach(video => {
+    videoList.forEach((video) => {
         const list = document.createElement('option');
         list.text = video.name;
         selectList.append(list);
-    })
+    });
 };
 
 $('#popup-close, #popup-layer').on('click', () => {
-    'use strict';
     $('#popup-layer, #popup-content, #popup-content-wide').remove();
 });
 
 const Player = new Array(30);
-const onPlayerReady = event => {
-    'use strict';
+const onPlayerReady = (event: any) =>
     setTimeout(() => {
-        event.target.pauseVideo();
+        event?.target?.pauseVideo();
     }, 1000);
-};
 
-const PlayerStart = player => {
-    'use strict';
+const PlayerStart = (player: YouTubePlayer) => {
     player.playVideo();
 };
 
 $(document).on('click', '.selectBtn', function () {
-    'use strict';
     const div = $(this).closest('div');
     const divId = div.attr('id');
+    if (!divId) return;
     const num = Number(divId.replace('video', ''));
 
     const video = videoListJson[$(this).prev('select').prop('selectedIndex')];
-    Player[num] = new YT.Player(divId, {
+    Player[num] = YTPlayer(divId, {
         width: div.width(),
         height: div.height(),
         videoId: video.id,
         playerVars: {
             start: video.start,
             end: video.end,
-            control: 0,
+            controls: 0,
             rel: 0,
             autoplay: 1,
             disablekb: 1,
             fs: 0,
-            enablejsapi: 1
+            enablejsapi: 1,
         },
         events: {
-            onReady: onPlayerReady
+            ready: onPlayerReady
         }
     });
-    const time = alarmDate - (video.fit - video.start) * 1000 - nowDate - 1500;
+
+    const time = alarmDate.getTime() - (video.fit - video.start) * 1000 - nowDate.getTime() - 1500;
     console.log(`ID: ${divId} / Time: ${time}ms`);
     setTimeout(() => {
         PlayerStart(Player[num]);
@@ -159,7 +148,6 @@ $(document).on('click', '.selectBtn', function () {
 });
 
 $('#vol-mute').on('click', function () {
-    'use strict';
     for (const i of Player) {
         if (typeof i === 'undefined') {
             continue;
@@ -173,7 +161,6 @@ $('#vol-mute').on('click', function () {
 });
 
 $('#vol-50').on('click', function () {
-    'use strict';
     for (const i of Player) {
         if (typeof i === 'undefined') {
             continue;
@@ -184,7 +171,6 @@ $('#vol-50').on('click', function () {
 });
 
 $('#vol-max').on('click', function () {
-    'use strict';
     for (const i of Player) {
         if (typeof i === 'undefined') {
             continue;
@@ -194,7 +180,7 @@ $('#vol-max').on('click', function () {
     }
 });
 
-const divVideoId = [...Array(30).keys()].map(i => i + 8);
+const divVideoId = [...Array(30).keys()].map((i) => i + 8);
 let idLen = divVideoId.length;
 while (idLen) {
     const j = Math.floor(Math.random() * idLen);
@@ -205,22 +191,22 @@ while (idLen) {
 
 const grid = $('#grid');
 
-const addItem = selector => {
-    'use strict';
-    $(selector).after(`<div class="item"><div class="selectDiv" id="video${divVideoId[0]}"><select class="selectList"></select><button class="selectBtn">決定</button></div></div>`);
+const addItem = (selector: string) => {
+    $(selector).after(
+        `<div class="item"><div class="selectDiv" id="video${divVideoId[0]}"><select class="selectList"></select><button class="selectBtn">決定</button></div></div>`
+    );
     divVideoId.shift();
 };
 
-const removeItem = selector => {
-    'use strict';
+const removeItem = (selector: string) => {
     const item = $(selector);
     const videoId = item.children('.selectDiv').attr('id');
+    if (!videoId) return;
     divVideoId.push(parseInt(videoId.replace('video', '')));
     item.remove();
 };
 
-const setGridCss = (type, num) => {
-    'use strict';
+const setGridCss = (type: 'col' | 'row', num: number) => {
     let name;
     switch (type) {
         case 'col':
@@ -234,14 +220,12 @@ const setGridCss = (type, num) => {
 };
 
 const getColRowNum = () => {
-    'use strict';
     const colNum = grid.css('grid-template-columns').split(' ').length;
     const rowNum = grid.css('grid-template-rows').split(' ').length;
     return {colNum, rowNum};
 };
 
 $('#grid-col-add').on('click', function () {
-    'use strict';
     const {colNum, rowNum} = getColRowNum();
     for (let i = 0; i < rowNum; i++) {
         const j = colNum * (i + 1) + i;
@@ -252,7 +236,6 @@ $('#grid-col-add').on('click', function () {
 });
 
 $('#grid-row-add').on('click', function () {
-    'use strict';
     const {colNum, rowNum} = getColRowNum();
     for (let i = 0; i < colNum; i++) {
         addItem('.item:last');
@@ -262,18 +245,16 @@ $('#grid-row-add').on('click', function () {
 });
 
 $('#grid-col-rm').on('click', function () {
-    'use strict';
     const {colNum} = getColRowNum();
     if (colNum > 1) {
         $(`.item:nth-child(${colNum}n)`).each(function () {
-            removeItem(this);
+            removeItem(this.id);
         });
         setGridCss('col', colNum - 1);
     }
 });
 
 $('#grid-row-rm').on('click', function () {
-    'use strict';
     const {colNum, rowNum} = getColRowNum();
     if (rowNum > 1) {
         for (let i = 0; i < colNum; i++) {
